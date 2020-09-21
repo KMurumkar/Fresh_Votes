27989a1bbf0ba01737be2459880dda7d360e17e4
Shubham PC <Shubham PC@Shubham>
2020-09-21 11:24:14 +0530

    adding some changes

---
+++ FreshVotes/src/main/java/com/freshvotes/domain/MyTable.java
@@ -0,0 +1,38 @@
+package com.freshvotes.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "InitTable")
+public class MyTable {
+	
+	@Id
+	private Integer id;
+	
+	
+	private String name;
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+
+}
